151f51e056bcf44656408e054360bb8df49d2721
Prexede <carlos.silva@estudante.ufscar.br>
2021-10-01 22:10:35 -0300

    Att/Add
    Frequencia de corte inferior e superior ds mosfets ai

--- MatLab/Funçoes/MosfetDeple.m
+++ MatLab/Funçoes/MosfetDeple.m
@@ -1,17 +1,20 @@
-function [Avt,Av,Zin,Zout,Vgs,Id] = MosfetDeple(Idss,Vp,Rd,Rs,Rl,Rf,Rg1,Rg2,Vd,Tipo)
+function [Avt,Av,Zin,Zout,Vgs,Id,FIci,FIco,FIcs,FSci,FSco] = MosfetDeple(Idss,Vp,Rd,Rs,Rl,Rf,Rg1,Rg2,Vd,Ci,Co,Cs,Cgd,Cgs,Cds,Tipo)
 
-%   Descobre o Id/Vgs de acordo com a polarização dele
+%   
 %  
 %
 %   Sintaxe:
 %
-%   [Id,Vds,Vgs] = MosfetDeple(Idss,Vp,Rd,Rs,R1,R2,Vd,Tipo)
-%
-%
-%   Observações:
-%
+%   [Avt,Av,Zin,Zout,Vgs,Id,FIci,FIco,FIcs,FSci,FSco] = MosfetDeple(Idss,Vp,Rd,Rs,Rl,Rf,Rg1,Rg2,Vd,Ci,Co,Cs,Cgd,Cgs,Cds,Tipo)
 %   
-
+%   Exemplo:
+%
+%   [Avt,Av,Zin,Zout,Vgs,Id,FIci,FIco,FIcs,FSci,FSco] = MosfetDeple(60E-3,-1.45,820,220,10000,1000,5.6E6,510000,24,220E-9,10E-6,560E-6,60E-12,0,0,'Div')
+%   
+%   Observações:
+% 
+%   Rf = Rsource 
+%   Vd = Vcc
 %
 %   Dependências:
 %
@@ -29,19 +32,39 @@ switch Tipo
       
     case 'Div'
         Vg = ((Rg2*Vd)/(Rg1+Rg2));
-        eq = x == Idss * (1 - (Vg - (x*Rs))/Vp)^2;   
-        Id = solve(eq,x);
+        eq = x == Idss * (1 - (Vg - (x*Rs))/Vp)^2; 
+        Id = vpasolve(eq,x);
+        
         Vgs = Vg - (Id*Rs);
         Vds = Vd - (Id*(Rs+Rd));
         
+    if  Vgs(1) > Vp
+        Vgs = Vgs(1);
+        
+    else
+        Vgs = Vgs(2);
+    end
+            
         
         Zin = ((Rg1*Rg2)/(Rg1+Rg2));
         Zout = Rd;
         Gm0 = (2*Idss)/(abs(Vp));
         Gm = Gm0 * ( 1 - (Vgs/Vp));
         par = (Rl*Rd)/(Rl+Rd);
-        Av = (-Gm*Rd);
+        Av = (-Gm*par);
         %Av = (-Gm*(par))/(1 + (Gm*(Rs)));
         Avt = Av*(Zin/(Rf+Zin));
+        
+        FIci = (1/(2*pi*(Rf+(Zin))*Ci));    %Frequencia corte inferior capacitor de entrada
+        FIco = (1/(2*pi*(Rd+Rl)*Co));       %Frequencia corte inferior capacitor de saida
+        Req2 = (((1/Gm)*Rs)/((1/Gm)+Rs));
+        FIcs = double(1/(2*pi*Req2*Cs));    %Frequencia corte inferior capacitor da fonte/Source
+    
+        Cot = (Cds+((1-(1/Av))*Cgd));
+        Cit = (Cgs+((1-(Av))*Cgd));
+    
+        FSco = double(1/(2*pi*par*Cot));
+        Req3 = ((Zin*Rf)/(Zin+Rf));
+        FSci = double(1/(2*pi*Req3*Cit));
        
 end
--- MatLab/Funçoes/MosfetInt.m
+++ MatLab/Funçoes/MosfetInt.m
@@ -1,17 +1,22 @@
-function [Id,Vds,Vgs,Zin,Zout,Av,Avt] = MosfetInt(k,Vt,Vd,Rg1,Rg2,Rd,Rs,Rf,Rl,Tipo)
+function [Id,Vds,Vgs,Zin,Zout,Av,Avt,FIci,FIco,FIcs,FSci,FSco] = MosfetInt(k,Vt,Vd,Rg1,Rg2,Rd,Rs,Rf,Rl,Ci,Co,Cs,Cgd,Cgs,Cds,Tipo)
 
-%   Descobre o Id/Vgs de acordo com a polarização dele
+%   
 %  
 %
 %   Sintaxe:
 %
-%   [Id,Vds] = MosfetInt(k,Vt,Vd,Rg1,Rg2,Rd,Rs,Tipo)
+%   [Id,Vds,Vgs,Zin,Zout,Av,Avt,FIci,FIco,FIcs,FSci,FSco] = MosfetInt(k,Vt,Vd,Rg1,Rg2,Rd,Rs,Rf,Rl,Ci,Co,Cs,Cgd,Cgs,Cds,Tipo)
 %
 %   Observações:
-%
-%   
+%   Rf = Rsource 
+%   Vd = Vcc
 %   Esponja chatão 
 %
+%   Exemplo:
+%
+%   
+%  [Id,Vds,Vgs,Zin,Zout,Av,Avt,FIci,FIco,FIcs,FSci,FSco] = MosfetInt(4.5,3,25,5.6E6,1.1E6,250,20,600,4700,100E-9,47E-6,6800E-6,60E-12,20E-12,0,'Div')
+% 
 %   Dependências:
 %
 %  
@@ -22,18 +27,41 @@ function [Id,Vds,Vgs,Zin,Zout,Av,Avt] = MosfetInt(k,Vt,Vd,Rg1,Rg2,Rd,Rs,Rf,Rl,Ti
 %   Última modificação:  
 
 syms x
+
 switch Tipo
     case 'Div' 
     Vg = (Rg2*Vd)/(Rg1+Rg2);
     eq = (Rs*k)*(x^2) + (1 - (2*Rs*k*Vt))*x + (((Vt^2)*Rs*k)- Vg);
-    Vgs = solve(eq,x);
+    Vgs = vpasolve(eq,x);
+  
+    
+    if  Vgs(1) > Vt
+        Vgs = Vgs(1);
+        
+    else
+        Vgs = Vgs(2);
+    end
+    
     Id = (Vg - Vgs)/Rs;
     Vds = Vd - (Id*(Rs+Rd));
     Zin = ((Rg1*Rg2)/(Rg1+Rg2));
     Zout = Rd;
+ 
     Gm = 2*k*(Vgs-Vt);
     par = (Rd*Rl)/(Rd+Rl);
     %Av = (-Gm*(par))/(1 + (Gm*Rs));
-    Av = (-Gm*Rd);
+    Av = (-Gm*par);
     Avt = Av*(Zin/Rf+Zin);
+    
+    FIci = (1/(2*pi*(Rf+(Zin))*Ci));    %Frequencia corte inferior capacitor de entrada
+    FIco = (1/(2*pi*(Rd+Rl)*Co));       %Frequencia corte inferior capacitor de saida
+    Req2 = (((1/Gm)*Rs)/((1/Gm)+Rs));
+    FIcs = double(1/(2*pi*Req2*Cs));    %Frequencia corte inferior capacitor da fonte/Source
+    
+    Cot = (Cds+((1-(1/Av))*Cgd));
+    Cit = (Cgs+((1-(Av))*Cgd));
+    
+    FSco = double(1/(2*pi*par*Cot));
+    Req3 = ((Zin*Rf)/(Zin+Rf));
+    FSci = double(1/(2*pi*Req3*Cit));
 end
