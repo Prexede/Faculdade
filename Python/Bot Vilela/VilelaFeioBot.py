e1ca60a3ef81d62f440a6662fae534fc0ad7cdf9
Prexede <carlos.silva@estudante.ufscar.br>
2021-12-27 12:16:09 -0300

    Att/Add
    Bot vilela telegram // Treino Altium Red dragons

---
+++ Python/Bot Vilela/VilelaFeioBot.py
@@ -0,0 +1,20 @@
+import telebot
+#from datetime import datetime
+#import time
+#import schedule
+
+#https://api.telegram.org/bot<5038538002:AAFaO4rNT99tdGrHc262AXEwZd7geJ62QQ8>/getUpdates
+Chave_API = "5038538002:AAFaO4rNT99tdGrHc262AXEwZd7geJ62QQ8"
+bot = telebot.TeleBot(Chave_API)
+bot.send_message(-781789851,"Ele e muito feio veio")
+
+#schedule.every(5).seconds.do(MsgBot)
+
+#while 1:
+    #schedule.run_pending()
+    #time.sleep(4)
+
+    
+
+
+  
---
+++ Python/Bot Vilela/lambda_function.py
@@ -0,0 +1,8 @@
+import json
+import telebot
+import pandas as pd
+import time
+
+Chave_API = "5038538002:AAFaO4rNT99tdGrHc262AXEwZd7geJ62QQ8"
+bot = telebot.TeleBot(Chave_API)
+bot.send_message(-781789851,"Ele e muito feio veio")
